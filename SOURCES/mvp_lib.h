1193dbdb10c2aa8f40c0242855d5ccd1a935b78a
Dorian <31416866+dorianTvo@users.noreply.github.com>
2020-10-10 22:24:03 +0200

    Update mvp_lib.h

--- SOURCES/mvp_lib.h
+++ SOURCES/mvp_lib.h
@@ -6,4 +6,4 @@ char* getWithoutSlashFromPath(char* path);
 char* getWithSlashFromPath(char* path);
 int mvOrCpOneFile(char* Spath, char* Dpath, int choice);
 int mvOrCpOneFolder(char* Spath, char* Dpath, int choice);
-char* concatanation(const char *s1, const char *s2);
+char* concatenation(const char *s1, const char *s2);
